dd062900b729ae595ec2c513310bd320ebffa39a
V-ranjan <vivek1993ranjan@gmail.com>
2025-02-23 17:31:43 +0530

    Chapter-4: Bit of Inline Style in JSX

--- app.js
+++ app.js
@@ -12,44 +12,49 @@ import ReactDOM from "react-dom/client";
 //   "Hello World from React CDN!"
 // );
 
-
 //Ways of writing functional component:
 
 const number = 1000;
 
-const HeadingComponent = () => <h1 id="headingComponent">This is a fucntional component</h1>;
+const styleCard = {
+  color: "Purple",
+};
+
+const HeadingComponent = () => (
+  <h1 id="headingComponent">This is a fucntional component</h1>
+);
 const HeadingComponent2 = () => (
-  <h1 id="headingComponent">
+  <h1 id="headingComponent" style={styleCard}>
     This is also a way of writing fucntional component
   </h1>
 );
 const HeadingComponent3 = () => {
   return (
-    <h1 id="headingComponent">
+    <h1 id="headingComponent" style={{ color: "blue" }}>
       This is most common way of writing fucntional component
     </h1>
   );
 };
 
-const HeadingComponent4 = function(){
+const HeadingComponent4 = function () {
   return (
     <h1 id="headingComponent">
-     {number} : This is the way of using old function syntax without using Arrow function 
-     {console.log("Any Javascript can be used here")}
+      {number} : This is the way of using old function syntax without using
+      Arrow function
+      {console.log("Any Javascript can be used here")}
     </h1>
   );
 };
-
 
 // Instead of React.createElement we will use the JSX which Babel will transpile
 const heading = (
   <div id="heading" className="headingClass">
     Now onwards We will use JSX!
-    <HeadingComponent/>
-    <HeadingComponent2/>
-    <HeadingComponent3/>
-    <HeadingComponent4/>
-      </div>
+    <HeadingComponent />
+    <HeadingComponent2 />
+    <HeadingComponent3 />
+    <HeadingComponent4 />
+  </div>
 );
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
