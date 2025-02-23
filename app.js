e94eed8ffcd800f1b75de2b63264bcfe99966eb2
V-ranjan <vivek1993ranjan@gmail.com>
2025-02-23 16:30:03 +0530

    Chapter-2.a : Using JSX intead React.CreateElement

--- app.js
+++ app.js
@@ -6,11 +6,15 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 
-const heading = React.createElement(
-  "h1",
-  { id: "heading" },
-  "Hello World from React CDN!"
-);
+// const heading = React.createElement(
+//   "h1",
+//   { id: "heading" },
+//   "Hello World from React CDN!"
+// );
+
+// Instead of React.createElement we will use the JSX which Babel will transpile 
+const heading = <h1 id = "heading">Now onwards We will use JSX!</h1>
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
 
